99a051740aec48c2c2c305c70791427d0f1d1ce1
Eric Peters <eric@chariots.es>
2024-09-30 11:49:43 +0200

    18.0.0.3 new estate.property fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,4 +7,20 @@ class RealEstate(models.Model):
 
     # this is creating fields in the table
     name = fields.Char(default="House", required=True)
-    price = fields.Float()
+    description = fields.Text()
+	postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+	selling_price = fields.Float()
+	bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+	garden = fields.Boolean()
+	garden_area = fields.Integer()
+	garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('n', 'North'), ('ne', 'Northeast'), ('e', 'East'), ('se', 'Southeast'), ('s', 'South'), 
+        ('sw', 'Southwest'), ('w', 'West'), ('nw', 'Northwest')],
+        help="Denotes which direction the garden is facing"
+        )
