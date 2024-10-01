712e707f500e6922f6557feebdbdb11778a4c5d7
Eric Peters <eric@chariots.es>
2024-10-01 14:47:27 +0200

    18.0.0.23

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -14,4 +14,19 @@ class RealEstate(models.Model):
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
     validity = fields.Integer(default = 7, required=True)
-    date_deadline = fields.Date()
+    date_deadline = fields.Date(compute="_compute_deadline", inverse="_inverse_deadline"))
+
+    @api.depends("validity")
+    def _compute_deadline(self):
+        for record in self:
+            if not record.create_date:
+                record.date_deadline = fields.Date.add(value=fields.Date.today(), record.validity)) 
+            else:
+                record.date_deadline = fields.Date.add(value=record.create_date, record.validity)) 
+
+    def _inverse_deadline(self):
+        for record in self:
+            if not record.create_date:
+                record.validity = (record.date_deadline - fields.Date.today()).days
+            else:
+                record.validity = (record.date_deadline - record.create_date).days
